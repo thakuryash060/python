9afbdb41f8bcf2b110ad29c2b5d44d80e2fb129c
Yash Thakur <thakuryash060@gmail.com>
2024-01-13 02:15:31 +0530

    HI

---
+++ c1.py
@@ -0,0 +1,41 @@
+#a = str(input())
+#lists= ["apple","mango","orange","cherry"]
+#list = ["yash","Thala","boomboom","virat","yash"]
+"""if a in list:
+    print("Okay")
+else:
+    print("check")
+    """
+#list.extend(lists)
+#print(list)
+#print(lists)
+#list.remove("yash")
+#list.pop(1)
+#for x in list:
+    #print(x,"thakur")
+#list.sort(reverse=True)
+#print(list)
+#list.sort(key=str.upper)
+#let = list.copy()
+
+#t1 = ("yash","rohit","virat")
+#t2 = ("sachin",)
+#t1 += t2
+#print(t1)
+#y1 = list(t1)
+#y1.remove("sachin")
+#t1 = tuple(y1)
+#del(t1)
+#print(t1)
+
+#(thakur,sharma,kohli) = t1
+#print(thakur)
+#for x in t1:
+    #print(x)
+#i=0
+#while i<len(t1):
+    #print(t1[i])
+    #i=i+1
+
+
+
